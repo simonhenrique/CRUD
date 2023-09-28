a25048a576b5f4a55404e7b3022ce737461f581a
Simon Henrique <simon_henrique16@outlook.com>
2023-09-28 13:23:30 -0300

    Configuração do Banco de dado e da Tabela Veículo

---
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informa o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informa a placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informa o Ano de Fabricação!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informa o Ano do modelo!")]
+        public int AnoModelo { get; set; }
+    }
+}
